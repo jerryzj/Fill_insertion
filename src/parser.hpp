eb5a3b8a37160a1263106867e01aae54b3afc776
danielyang2055 <ydaniel821@gmail.com>
2018-05-23 20:50:59 +0800

    create layout class

--- src/parser.hpp
+++ src/parser.hpp
@@ -46,6 +46,34 @@ class readrule{
         // variables
         int rule_num;   // number of rules
         vector<rule> rules;
+}; 
+
+class layout{
+public:
+    enum NetType {Normal, Fill};
+    struct rectangle{
+        int bl_x;   // buttom left X
+        int bl_y;   // buttom left y
+        int tr_x;   // top right x
+        int tr_y;   // top right y
+    };
+    struct net{
+        int poly_id;
+        int net_id;
+        int layer;
+        rectangle rect;
+    };
+    layout();
+    void read_file(char* filename);
+
+    rectangle boundary;
+    // need window size to set bin size: bin = 0.5 * window 
+    int bin_size; 
+    // 3D array layer*row*col
+    // layer = 9 (L0 to L8), need index conversion 
+    
+
 };
+
 
 #endif
