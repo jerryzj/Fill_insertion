35965274a7dd750f2ddf66c6d6d8f041c494aafa
danielyang2055 <ydaniel821@gmail.com>
2018-05-24 15:46:52 +0800

    add def of some rectangle functions

--- src/rectangle.hpp
+++ src/rectangle.hpp
@@ -9,7 +9,7 @@ using namespace std;
 
 
 class Rectangle{
-    public:
+public:
     // constructure
     Rectangle(){}  
     // constructure with initialize 
@@ -19,10 +19,14 @@ class Rectangle{
     Rectangle(const Rectangle &_r) 
             {bl_x = _r.bl_x; bl_y = _r.bl_y; tr_x = _r.tr_x; tr_y = _r.tr_y;}
 
-    friend class Layout;   // class Layout can access private member
     void set_rectangle(int _bl_x, int _bl_y, int _tr_x, int _tr_y);
- 
-    private: 
+    int area();
+
+    friend class Layout;   // class Layout can access private member
+    friend int area_overlap(const Rectangle &_r1, const Rectangle &_r2);
+    friend int rect_distance(const Rectangle &_r1, const Rectangle &_r2);
+    
+private: 
     int bl_x;   // buttom left X
     int bl_y;   // buttom left y
     int tr_x;   // top right x
