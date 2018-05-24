fab121b9262f7d53c3525c94c68825840c075416
danielyang2055 <ydaniel821@gmail.com>
2018-05-24 15:57:38 +0800

    add rectangle area

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -8,3 +8,13 @@ void Rectangle::set_rectangle(int _bl_x, int _bl_y, int _tr_x, int _tr_y)
     tr_x = _tr_x;
     tr_y = _tr_y;
 }
+
+int Rectangle::area()
+{
+    int area = (tr_x-bl_x)*(tr_y-bl_y);
+
+    if (area <= 0)
+        cerr << "The rectangle has area <=0 " << endl;
+    
+    return area;
+}
